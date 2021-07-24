9e625663a331b8e44b25a9889ce3bc353a40c0ed
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-24 13:06:04 +0530

    [Ravina]Add: Main Program File

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DAY_15_BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Binary Search Tree!");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+            Console.ReadKey();
+
+        }
+    }
+}
